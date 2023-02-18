2298509c93f86f79eb217f91578186ec33d172fa
Mithun <mithun.mohandas@aspiresys.com>
2023-02-18 14:16:40 +0530

    Added another @Around aspect: For the controller methods

--- src/main/kotlin/io/mcm/springbootwithkotlin/aspects/LoggingAspect.kt
+++ src/main/kotlin/io/mcm/springbootwithkotlin/aspects/LoggingAspect.kt
@@ -7,6 +7,10 @@ import org.aspectj.lang.annotation.Pointcut
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import javax.servlet.http.HttpServletRequest
 
 @Aspect
 @Component
@@ -20,17 +24,37 @@ class LoggingAspect {
     }
 
     @Around("logServiceCallsPointcut()")
-    fun aroundServiceCallsExecution(proceedPoint: ProceedingJoinPoint) {
-        LOGGER.info("Before execution of service method")
-        println(proceedPoint.args)
-        println(proceedPoint.kind)
-        println(proceedPoint.signature)
-        println(proceedPoint.sourceLocation)
-        println(proceedPoint.target)
-        println(proceedPoint.staticPart)
+    fun aroundServiceCallsExecution(proceedPoint: ProceedingJoinPoint): Any? {
+        println("${proceedPoint.signature} invoked with arguments ${proceedPoint.args.contentToString()}")
+        val result: Any = proceedPoint.proceed()
+        println("${proceedPoint.signature} completed with result: $result")
+        /*
+        proceedPoint.args: [2]
+        proceedPoint.kind: method-execution
+        proceedPoint.signature: BooksResponse io.mcm.springbootwithkotlin.service.BooksService.findById(long)
+        proceedPoint.sourceLocation: org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint$SourceLocationImpl@57c32c19
+        proceedPoint.target: io.mcm.springbootwithkotlin.service.BooksService@681d918c
+        proceedPoint.staticPart: execution(BooksResponse io.mcm.springbootwithkotlin.service.BooksService.findById(long))
+        */
+        return result
+    }
+    @Pointcut("execution(* io.mcm.springbootwithkotlin.controller.*.*(..))")
+    fun logControllerCallsPointcut() {
+    }
 
-        proceedPoint.proceed()
-
-        LOGGER.info("proceedPoint.signature:{}", proceedPoint.signature)
+    @Around("logControllerCallsPointcut()")
+    fun aroundControllerCallsExecution(proceedPoint: ProceedingJoinPoint): Any? {
+        println("${proceedPoint.signature} invoked with arguments ${proceedPoint.args.contentToString()}")
+        val result: Any = proceedPoint.proceed()
+        println("${proceedPoint.signature} completed with result: $result")
+        /*
+        proceedPoint.args: [2]
+        proceedPoint.kind: method-execution
+        proceedPoint.signature: BooksResponse io.mcm.springbootwithkotlin.service.BooksService.findById(long)
+        proceedPoint.sourceLocation: org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint$SourceLocationImpl@57c32c19
+        proceedPoint.target: io.mcm.springbootwithkotlin.service.BooksService@681d918c
+        proceedPoint.staticPart: execution(BooksResponse io.mcm.springbootwithkotlin.service.BooksService.findById(long))
+        */
+        return result
     }
 }
