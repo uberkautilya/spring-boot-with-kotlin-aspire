1681f4a64b7fb526399f380123182905ae40b0f9
Mithun <mithuncm@gmail.com>
2023-02-20 12:30:43 +0530

    LoggingAspect replaces LOGGERs at controller and service for method entry and exit

--- src/main/kotlin/io/mcm/springbootwithkotlin/aspects/LoggingAspect.kt
+++ src/main/kotlin/io/mcm/springbootwithkotlin/aspects/LoggingAspect.kt
@@ -4,20 +4,12 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import org.springframework.web.context.request.RequestAttributes
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.request.ServletRequestAttributes
-import javax.servlet.http.HttpServletRequest
 
 @Aspect
 @Component
 class LoggingAspect {
-    companion object {
-        val LOGGER: Logger = LoggerFactory.getLogger(LoggingAspect::class.java)
-    }
 
     @Pointcut("execution(* io.mcm.springbootwithkotlin.service.*.*(..))")
     fun logServiceCallsPointcut() {
@@ -25,9 +17,10 @@ class LoggingAspect {
 
     @Around("logServiceCallsPointcut()")
     fun aroundServiceCallsExecution(proceedPoint: ProceedingJoinPoint): Any? {
-        println("${proceedPoint.signature} invoked with arguments ${proceedPoint.args.contentToString()}")
+        val serviceLogger = LoggerFactory.getLogger(proceedPoint.signature.declaringType)
+        serviceLogger.info("${proceedPoint.signature} invoked with arguments \n${proceedPoint.args.contentToString()}")
         val result: Any = proceedPoint.proceed()
-        println("${proceedPoint.signature} completed with result: $result")
+        serviceLogger.info("${proceedPoint.signature} completed with result: \n$result")
         /*
         proceedPoint.args: [2]
         proceedPoint.kind: method-execution
@@ -38,23 +31,17 @@ class LoggingAspect {
         */
         return result
     }
+
     @Pointcut("execution(* io.mcm.springbootwithkotlin.controller.*.*(..))")
     fun logControllerCallsPointcut() {
     }
 
     @Around("logControllerCallsPointcut()")
     fun aroundControllerCallsExecution(proceedPoint: ProceedingJoinPoint): Any? {
-        println("${proceedPoint.signature} invoked with arguments ${proceedPoint.args.contentToString()}")
+        val controllerLogger = LoggerFactory.getLogger(proceedPoint.signature.declaringType)
+        controllerLogger.info("${proceedPoint.signature} invoked with arguments \n${proceedPoint.args.contentToString()}")
         val result: Any = proceedPoint.proceed()
-        println("${proceedPoint.signature} completed with result: $result")
-        /*
-        proceedPoint.args: [2]
-        proceedPoint.kind: method-execution
-        proceedPoint.signature: BooksResponse io.mcm.springbootwithkotlin.service.BooksService.findById(long)
-        proceedPoint.sourceLocation: org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint$SourceLocationImpl@57c32c19
-        proceedPoint.target: io.mcm.springbootwithkotlin.service.BooksService@681d918c
-        proceedPoint.staticPart: execution(BooksResponse io.mcm.springbootwithkotlin.service.BooksService.findById(long))
-        */
+        controllerLogger.info("${proceedPoint.signature} completed with result: \n$result")
         return result
     }
 }
--- src/main/kotlin/io/mcm/springbootwithkotlin/controller/BooksController.kt
+++ src/main/kotlin/io/mcm/springbootwithkotlin/controller/BooksController.kt
@@ -11,56 +11,56 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1/books")
 public class BooksController(val booksService: BooksService) {
-
+/*
     companion object {
         private val LOGGER = LoggerFactory.getLogger(BooksController::class.java)
-    }
+    }*/
 
     @PostMapping
     fun addBooks(@RequestBody booksRequest: BooksRequest): ResponseEntity<BooksResponse> {
-        LOGGER.info("BooksController:: addBooks({})", booksRequest)
+//        LOGGER.info("BooksController:: addBooks({})", booksRequest)
         val booksResponse: BooksResponse = booksService.save(booksRequest.bookList!!)
         return ResponseEntity.ok(booksResponse)
     }
 
     @PostMapping("/filtered")
     fun getBookFiltered(@RequestBody booksRequest: BooksRequest): ResponseEntity<BooksResponse> {
-        LOGGER.info("BooksController.getBookFiltered: booksRequest: $booksRequest")
+//        LOGGER.info("BooksController.getBookFiltered: booksRequest: $booksRequest")
         val booksResponse: BooksResponse = booksService.getBookFiltered(booksRequest)
         return ResponseEntity.ok(booksResponse)
     }
 
     @GetMapping
     fun findAll(): ResponseEntity<BooksResponse> {
-        LOGGER.info("BooksController:: findAll() method")
+//        LOGGER.info("BooksController:: findAll() method")
         val booksFound:BooksResponse = booksService.findAll()
         return ResponseEntity<BooksResponse>(booksFound, HttpStatus.OK)
     }
 
     @GetMapping("/{id}")
     fun findById(@PathVariable("id") id: Long): ResponseEntity<BooksResponse> {
-        LOGGER.info("BooksController:: findById({})", id)
+//        LOGGER.info("BooksController:: findById({})", id)
         val bookById: BooksResponse = booksService.findById(id)
         return ResponseEntity<BooksResponse>(bookById, HttpStatus.OK)
     }
 
     @PutMapping
     fun updateBooks(@RequestBody booksRequest: BooksRequest): ResponseEntity<BooksResponse> {
-        LOGGER.info("BooksController:: updateBooks({})", booksRequest)
+//        LOGGER.info("BooksController:: updateBooks({})", booksRequest)
         val updatedBooksResponse: BooksResponse = booksService.updateBooks(booksRequest.bookList!!)
         return ResponseEntity<BooksResponse>(updatedBooksResponse, HttpStatus.OK)
     }
 
     @DeleteMapping
     fun deleteBooks(@RequestBody bookIdsToDelete: List<Long>): ResponseEntity<BooksResponse> {
-        LOGGER.info("BooksController:: deleteBooks({})", bookIdsToDelete)
+//        LOGGER.info("BooksController:: deleteBooks({})", bookIdsToDelete)
         val booksDeletedResponse: BooksResponse = booksService.deleteBooks(bookIdsToDelete)
         return ResponseEntity<BooksResponse>(booksDeletedResponse, HttpStatus.OK)
     }
 
     @GetMapping("/property")
     fun getApplicationProperty(): ResponseEntity<String> {
-        LOGGER.info("BooksController:: getApplicationProperty()")
+//        LOGGER.info("BooksController:: getApplicationProperty()")
         val applicationProperty: String = booksService.getApplicationProperty()
         return ResponseEntity<String>(applicationProperty, HttpStatus.OK)
     }
